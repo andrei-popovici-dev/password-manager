d19d79fb10056634fadfa8188b5ebec17a76a030
Popovici Andrei Razvan <popovici.andrei2002@yahoo.com>
2025-11-01 22:13:56 +0200

    Added some buttons and text boxes to test functionality

--- FrontApp.py
+++ FrontApp.py
@@ -1,34 +1,41 @@
+from kivy.config import Config
+# make the app not resizeable to keep GUI looking clean
+Config.set('graphics', 'resizable', False)
+Config.set('graphics', 'width', '1000')
+Config.set('graphics', 'height', '700')
+
 from kivy.app import App
+from kivy.uix import layout
+from kivy.uix.behaviors import DragBehavior
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
+from TextInputApp import TextInputBox
 
-class PositionedPasswordManager(App):
+class PasswordManager(App):
     def build(self):
         layout = FloatLayout()
 
         Window.clearcolor = (0,0,1,1)
         # Label feedback
         self.message_label = Label(
-            text='',
+            text='Hello!',
             size_hint=(0.8, 0.1),
-            pos_hint={'x':0.1, 'y':0.85},
-            color=(1,1,1,1)
+            pos_hint={'x':0.3, 'y':0.75},
+            color=(1,1,1,1),
+            font_size = 24
         )
         layout.add_widget(self.message_label)
 
         # Username
-        self.username_input = self.create_text_input(0.2,0.65,'Username')
-
-        self.username_input.on_cursor()
+        self.username_input = TextInputBox(x = 0.05,y = 0.83,hint = 'Username')
 
         layout.add_widget(self.username_input)
 
         # Password
-        self.password_input = self.create_text_input(0.2,0.5,'Password')
+        self.password_input = TextInputBox(x = 0.05,y = 0.70,hint = 'Password',password = True)
         layout.add_widget(self.password_input)
 
         # Save Button
@@ -41,13 +48,13 @@ class PositionedPasswordManager(App):
         layout.add_widget(save_btn)
 
         # Show Password
-        show_btn = Button(
+        self.show_btn = Button(
             text='Show',
             size_hint=(0.25,0.1),
             pos_hint={'x':0.37, 'y':0.3}
         )
-        show_btn.bind(on_press=self.show_password)
-        layout.add_widget(show_btn)
+        self.show_btn.bind(on_press=self.show_password)
+        layout.add_widget(self.show_btn)
 
         # Generate
         generate_btn = Button(
@@ -65,28 +72,13 @@ class PositionedPasswordManager(App):
 
     def show_password(self, instance):
         self.password_input.password = not self.password_input.password
-
+        self.show_btn.text = 'Hide' if not self.password_input.password else 'Show'
+        
     def generate_password(self, instance):
         import random, string
         self.password_input.text = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
         self.message_label.text = 'Generated!'
 
-    def create_text_input(self,x,y,hint_text):
-        return TextInput(
-            hint_text=hint_text,
-            multiline=False,
-            background_normal='Photos/GroupedList.png',  # imagine ta când nu e selectat
-            background_active='Photos/GroupedList.png',  # imagine când e focusat
-            size_hint=(0.6, 0.1),
-            padding_y=(15, 15),  # sus, jos
-            padding_x=10,
-            font_size=24,
-            _cursor_blink=True,
-            cursor_color=(0, 0, 0, 1),
-            cursor_blink=True,
-            pos_hint={'x': x, 'y': y},
-            foreground_color=(0, 0, 0, 1),)  # text negru
-
 
 if __name__ == '__main__':
-    PositionedPasswordManager().run()
+    PasswordManager().run()
