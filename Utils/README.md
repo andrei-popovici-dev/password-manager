06200b096a9906930c80797e7f04d4f9b31e4934
Popovici Andrei Razvan <popovici.andrei2002@yahoo.com>
2025-12-19 19:32:38 +0200

    Made changes - now ready for merge

--- Utils/README.md
+++ Utils/README.md
@@ -1,16 +1,25 @@
 # Password Manager
-Descrierea aplicatiei: aplicatia este un password manager care stocheaza sub un singur utilizator mai multe conturi ale utilizatorului. Practic o persoana va trebui sa retina doar o singura parola pentru a avea acces la toate parolele sale. Aplicatia include si un analizator de parole si un generator de parole.
+Descrierea aplicatiei: aplicatia este un password manager care stocheaza sub un singur utilizator mai multe conturi ale utilizatorului. Practic o persoana va trebui sa retina doar o singura parola pentru a avea acces la toate parolele sale. Aplicatia include si un analizator de parole care analizeaza parolele stocate si cere utilizatorului sa le modifice astfel incat acestea sa fie mai puternice, dar si un generator de parole care creeaza parole din diferite caractere si de diferite marimi.
+
+Link github: https://github.com/andrei-popovici-dev/password-manager/
+
 Implementarea folosete:
-    1. BackApp.py care se ocupa de logica in sine;
-    2. crypto.py care se ocupa cu criptarea/decriptarea parolelor;
-    3. data.json in care stocam toate datele despre useri: numele si parola(criptate, parola pentru cont nu trebuie afisata niciodata, doar testata, in schimb parolele pentru diversele conturi ale userului trebuie afisate deci trebuie decriptate), si credentialele pentru diverse website-uri, care la randul lor vor retine: numele site-ului/ contului, user/mail/numar de telefon si parola.
-    4. FrontApp.py care reprezinta un API expus de BackApp.py catre frontend;
-    5. Frontend-ul alcatuit din: 
-    - index.html - fisierul principal care incarca toate datele din celalalte fisiere ale frontend-ului;
-    - views - fisier care contine alte fisiere html ale frontend-ului : login.html - contine interfata de login in cont ;dashboard.html - contine dashboard-ul(vizualizare parole slabe, vizualizare tabela de parole, generare de parole);
-    - js - fisierele javascript responsabile pentru logica intre Main.py si frontend
+
+1. BackApp.py care se ocupa de logica in sine;
+
+2. crypto.py care se ocupa cu criptarea/decriptarea parolelor;
+
+3. data.json in care stocam toate datele despre useri: numele si parola(criptate, parola pentru cont nu trebuie afisata niciodata, doar testata, in schimb parolele pentru diversele conturi ale userului trebuie afisate deci trebuie decriptate), si credentialele pentru diverse website-uri, care la randul lor vor retine: numele site-ului/ contului, user/mail/numar de telefon si parola.
+
+4. Main.py care reprezinta un API expus de BackApp.py catre frontend, dar si punctul de start al aplicatiei.
+
+5. Frontend-ul alcatuit din:
+   - index.html - fisierul principal care incarca toate datele din celalalte fisiere ale frontend-ului;
+   - views - director care contine alte fisiere html ale frontend-ului : login.html - contine interfata de login in cont ;dashboard.html - contine dashboard-ul(vizualizare parole slabe, vizualizare tabela de parole, generare de parole);
+   - js - fisierele javascript responsabile pentru logica intre Main.py si frontend, cat si unele functionalitati
 
 Utilizare: Aplicatia va fi rulata din Main.py
 
-Calin Stefan-Gabriel : a implementat criptarea parolelor, stocarea datelor despre utilizatori 
+Calin Gabriel-Stefan : a implementat criptarea parolelor, stocarea datelor despre utilizatori
+
 Popovici Andrei-Razvan : a implementat frontend-ul si bridge-ul dintre frontend si backend (Main.py)
--- js/dashboard_overview.js
+++ js/dashboard_overview.js
@@ -25,7 +25,7 @@ async function checkWeakPasswords() {
                     <td class="px-4 py-3 font-medium text-slate-800">${acc.website}</td>
                     <td class="px-4 py-3 text-slate-500">${acc.login}</td>
                     <td class="px-4 py-3 text-right">
-                        <button onclick="openEditModal(${acc.id})" 
+                        <button onclick="openEditModalOverview(${acc.id})" 
                             class="text-indigo-600 hover:text-indigo-800 text-xs font-bold border border-indigo-100 bg-indigo-50 px-2 py-1 rounded">
                             Fix
                         </button>
@@ -42,5 +42,108 @@ async function checkWeakPasswords() {
 
     } catch (error) {
         listBody.innerHTML = `<tr><td colspan="3" class="p-4 text-center text-red-500">Error scanning passwords.</td></tr>`;
+    }
+}
+
+
+
+async function handleFormSubmitOverview() {
+    const site = document.getElementById('new-site-overview').value;
+    const login = document.getElementById('new-login-overview').value;
+    const pass = document.getElementById('new-pass-overview').value;
+    const messageBox = document.getElementById('feedback-message-modal-overview');
+
+    if (!site || !login || !pass) {
+        messageBox.textContent = "Complete all fields!";
+        messageBox.style.color = "red";
+        return;
+    }
+
+    messageBox.textContent = "Processing...";
+    messageBox.style.color = "blue";
+
+    let success, message;
+
+    if (editingId === null) {
+        const response = await window.pywebview.api.add_new_credential(site, login, pass);
+        success = response[0];
+        message = response[1];
+    } else {
+
+        const response = await window.pywebview.api.update_credential(editingId, site, login, pass);
+        success = response[0];
+        message = response[1];
+    }
+
+    if (success) {
+        messageBox.textContent = message;
+        messageBox.style.color = "green";
+
+        loadCredentials();
+        setTimeout(() => {
+            closeModalOverview();
+        }, 500);
+    } else {
+        messageBox.textContent = message;
+        messageBox.style.color = "red";
+    }
+}
+
+async function toggleModalPasswordVisibilityOverview(index) {
+    const input = document.getElementById('new-pass-overview');
+    const icon = document.getElementById('eye-icon-pass-overview');
+
+    if (!input || !icon) return;
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.style.stroke = "#2563eb";
+
+        icon.innerHTML = `<path d="M9.88 9.88a3 3 0 1 0 4.24 4.24"></path><path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"></path><path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7c.44 0 .87-.03 1.28-.09"></path><line x1="2" y1="2" x2="22" y2="22"></line>`;
+    } else {
+        input.type = "password";
+        icon.style.stroke = "currentColor";
+
+        icon.innerHTML = `<path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path><circle cx="12" cy="12" r="3"></circle>`;
+    }
+}
+
+function closeModalOverview() {
+    document.getElementById('add-modal-overview').classList.add('hidden');
+    editingId = null;
+}
+
+async function openEditModalOverview(id) {
+    editingId = id;
+
+    website = document.getElementById(`website-${id}`).innerText
+    login = document.getElementById(`login-${id}`).innerText
+
+    document.getElementById('modal-title-overview').innerText = "Edit password";
+    document.getElementById('modal-save-btn-overview').innerText = "Update";
+
+    document.getElementById('new-site-overview').value = website;
+    document.getElementById('new-login-overview').value = login;
+
+    const passInput = document.getElementById('new-pass-overview');
+    passInput.type = 'password';
+    passInput.value = "Loading...";
+
+    const icon = document.getElementById('eye-icon-pass-overview');
+    if (icon) {
+        icon.style.stroke = "currentColor";
+        icon.innerHTML = `<path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path><circle cx="12" cy="12" r="3"></circle>`;
+    }
+    document.getElementById('add-modal-overview').classList.remove('hidden');
+
+    try {
+        const realPass = await window.pywebview.api.reveal_password(Number(id));
+        if (realPass !== "Error") {
+            passInput.value = realPass;
+        } else {
+            passInput.value = "";
+        }
+    } catch (e) {
+        passInput.value = "";
     }
 }
